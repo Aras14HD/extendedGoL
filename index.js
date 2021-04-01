2c9b30cefd4c2459224e184114c42f6fb268247f
Aras <jorimbk@gmail.com>
2021-04-01 20:59:11 +0200

    worker

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let rows = 50;
-let columns = 50;
+let rows = 200;
+let columns = 200;
 let cells = [];
 let pause = true;
 let speed = 60;
@@ -60,7 +60,7 @@ function initCells() {
   }
   window.setInterval(run, speed);
 }
-function toggleCell(cell) {
+async function toggleCell(cell) {
   let column = cell.target.className.split(" ")[1].replace("column-", "");
   let row = cell.target.className.split(" ")[2].replace("row-", "");
   switch (cells[column][row]) {
@@ -73,78 +73,20 @@ function toggleCell(cell) {
       cells[column][row] = x;
   }
 }
-function run() {
+async function run() {
   if (!pause) {
-    let tcells = [];
-    for (let column = 0; column < cells.length; column++) {
-      tcells.push([]);
-      for (let row = 0; row < cells[column].length; row++) {
-        let n = 0.0;
-        if (column > 0) {
-          if (row > 0) {
-            n += parseFloat(cells[column - 1][row - 1]);
-          } else {
-            n += parseFloat(cells[column - 1][rows - 1]);
-          }
-          n += parseFloat(cells[column - 1][row]);
-          if (row < rows - 1) {
-            n += parseFloat(cells[column - 1][row + 1]);
-          } else {
-            n += parseFloat(cells[column - 1][0]);
-          }
-        } else {
-          if (row > 0) {
-            n += parseFloat(cells[columns - 1][row - 1]);
-          } else {
-            n += parseFloat(cells[columns - 1][rows - 1]);
-          }
-          n += parseFloat(cells[columns - 1][row]);
-          if (row < rows - 1) {
-            n += parseFloat(cells[columns - 1][row + 1]);
-          } else {
-            n += parseFloat(cells[columns - 1][0]);
-          }
+    worker = new Worker("Worker.js");
+    worker.postMessage([cells, columns, rows]);
+    worker.onmessage = function (tcells) {
+      cells = tcells;
+      for (let column = 0; column < cells.length; column++) {
+        for (let row = 0; row < cells[column].length; row++) {
+          document.getElementById(
+            "cell-" + column + ":" + row
+          ).style.background =
+            "hsl(0, 0%, " + (-1 * cells[column][row] + 1) * 100 + "%)";
         }
-        if (row > 0) {
-          n += parseFloat(cells[column][row - 1]);
-        } else n += parseFloat(cells[column][rows - 1]);
-        if (row < rows - 1) {
-          n += parseFloat(cells[column][row + 1]);
-        } else n += parseFloat(cells[column][0]);
-        if (column < columns - 1) {
-          if (row > 0) {
-            n += parseFloat(cells[column + 1][row - 1]);
-          } else n += parseFloat(cells[column + 1][rows - 1]);
-          n += parseFloat(cells[column + 1][row]);
-          if (row < rows - 1) {
-            n += parseFloat(cells[column + 1][row + 1]);
-          } else n += parseFloat(cells[column + 1][0]);
-        } else {
-          if (row > 0) {
-            n += parseFloat(cells[0][row - 1]);
-          } else n += parseFloat(cells[0][rows - 1]);
-          n += parseFloat(cells[0][row]);
-          if (row < rows - 1) {
-            n += parseFloat(cells[0][row + 1]);
-          } else n += parseFloat(cells[0][0]);
-        }
-
-        if (cells[column][row] >= 0.5) {
-          tcells[column][row] = -0.5 * (n - 2.5) * (n - 2.5) + 1.125;
-        } else {
-          tcells[column][row] = -1 * (n - 3) * (n - 3) + 1;
-        }
-        if (tcells[column][row] < 0) tcells[column][row] = 0;
-        if (tcells[column][row] > 1) tcells[column][row] = 1;
       }
-    }
-
-    cells = tcells;
-    for (let column = 0; column < cells.length; column++) {
-      for (let row = 0; row < cells[column].length; row++) {
-        document.getElementById("cell-" + column + ":" + row).style.background =
-          "hsl(0, 0%, " + (-1 * cells[column][row] + 1) * 100 + "%)";
-      }
-    }
+    };
   }
 }
