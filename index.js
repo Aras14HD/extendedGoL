439fdda148863646f1e3b1ce4100f801c1fe559c
Aras <jorimbk@gmail.com>
2021-05-16 13:25:40 +0200

    refactoring and url params -settings through url -now using template literals -more acurate mouse pos -convolition overhaul

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-let rows = 200;
-let columns = 200;
-let cells = [];
+const url = new URL(window.location.href);
+const rows = url.searchParams.get("w") || 200;
+const columns = url.searchParams.get("w") || 200;
+const speed = url.searchParams.get("s") || 60;
+
 let pause = true;
-let speed = 60;
 let x = 1;
+let cells = [];
 for (let column = 0; column < columns; column++) {
   cells.push([]);
   for (let row = 0; row < rows; row++) {
@@ -14,20 +16,11 @@ console.log(cells);
 document.addEventListener("DOMContentLoaded", function () {
   let html = "";
   for (let column = 0; column < columns; column++) {
-    html += "<div class='column' id='column-" + column + "'>";
+    html += `<div class="column" id="column-${column}">`;
     for (let row = 0; row < rows; row++) {
-      html +=
-        "<div class='cell column-" +
-        column +
-        " row-" +
-        row +
-        "' id='cell-" +
-        column +
-        ":" +
-        row +
-        "'></div>";
+      html += `<div class="cell column-${column} row-${row}" id="cell-${column}:${row}"></div>`;
     }
-    html += "</div>";
+    html += `</div>`;
   }
   document.getElementById("pause").addEventListener("click", () => {
     pause = !pause;
@@ -63,14 +56,15 @@ function initCells() {
 }
 function toggleCell(e) {
   let canvas = document.getElementById("container").getContext("2d");
-  let xpos = e.layerX;
-  let ypos = e.layerY;
-  let column = Math.round(xpos / (800 / columns) - 800 / columns / 2) - 1;
-  let row = Math.round(ypos / (800 / rows) - 800 / rows / 2) - 1;
+  let pos = getMousePos(document.getElementById("container"), e);
+  let column = Math.round(pos.x / (800 / columns)) - 1;
+  let row = Math.round(pos.y / (800 / rows)) - 1;
+
   if (column > columns - 1) column = columns - 1;
   if (column < 0) column = 0;
   if (row > rows - 1) row = rows - 1;
   if (row < 0) row = 0;
+
   switch (cells[column][row]) {
     case x:
       cells[column][row] = 0;
@@ -78,7 +72,7 @@ function toggleCell(e) {
     default:
       cells[column][row] = x;
   }
-  canvas.fillStyle = "hsl(0, 0%, " + (-1 * cells[column][row] + 1) * 100 + "%)";
+  canvas.fillStyle = `hsl(0, 0%, ${(-1 * cells[column][row] + 1) * 100}%)`;
   canvas.fillRect(
     column * (800 / columns),
     row * (800 / rows),
@@ -86,61 +80,43 @@ function toggleCell(e) {
     800 / columns
   );
 }
-async function run() {
+function run() {
   if (!pause) {
+    //debug vars
     let y = [0, 0, 0, 0, 0];
+
     let tcells = [];
     for (let column = 0; column < cells.length; column++) {
       tcells.push([]);
       for (let row = 0; row < cells[column].length; row++) {
+        //sum of neighbors
         let n = 0.0;
-        if (column > 0) {
-          if (row > 0) {
-            n += parseFloat(cells[column - 1][row - 1]);
-          } else {
-            n += parseFloat(cells[column - 1][rows - 1]);
+
+        for (let x = -1; x < 2; x++) {
+          for (let y = -1; y < 2; y++) {
+            if (x != 0 || y != 0) {
+              let neighbor_column, neighbor_row;
+
+              if (x == -1 && column == 0) {
+                neighbor_column = columns - 1;
+              } else if (x == 1 && column == columns - 1) {
+                neighbor_column = 0;
+              } else neighbor_column = column + x;
+
+              if (y == -1 && row == 0) {
+                neighbor_row = rows - 1;
+              } else if (y == 1 && row == rows - 1) {
+                neighbor_row = 0;
+              } else neighbor_row = row + y;
+
+              if (
+                cells[neighbor_column] != undefined &&
+                cells[neighbor_column][neighbor_row] != undefined
+              ) {
+                n += parseFloat(cells[neighbor_column][neighbor_row]);
+              }
+            }
           }
-          n += parseFloat(cells[column - 1][row]);
-          if (row < rows - 1) {
-            n += parseFloat(cells[column - 1][row + 1]);
-          } else {
-            n += parseFloat(cells[column - 1][0]);
-          }
-        } else {
-          if (row > 0) {
-            n += parseFloat(cells[columns - 1][row - 1]);
-          } else {
-            n += parseFloat(cells[columns - 1][rows - 1]);
-          }
-          n += parseFloat(cells[columns - 1][row]);
-          if (row < rows - 1) {
-            n += parseFloat(cells[columns - 1][row + 1]);
-          } else {
-            n += parseFloat(cells[columns - 1][0]);
-          }
-        }
-        if (row > 0) {
-          n += parseFloat(cells[column][row - 1]);
-        } else n += parseFloat(cells[column][rows - 1]);
-        if (row < rows - 1) {
-          n += parseFloat(cells[column][row + 1]);
-        } else n += parseFloat(cells[column][0]);
-        if (column < columns - 1) {
-          if (row > 0) {
-            n += parseFloat(cells[column + 1][row - 1]);
-          } else n += parseFloat(cells[column + 1][rows - 1]);
-          n += parseFloat(cells[column + 1][row]);
-          if (row < rows - 1) {
-            n += parseFloat(cells[column + 1][row + 1]);
-          } else n += parseFloat(cells[column + 1][0]);
-        } else {
-          if (row > 0) {
-            n += parseFloat(cells[0][row - 1]);
-          } else n += parseFloat(cells[0][rows - 1]);
-          n += parseFloat(cells[0][row]);
-          if (row < rows - 1) {
-            n += parseFloat(cells[0][row + 1]);
-          } else n += parseFloat(cells[0][0]);
         }
 
         if (cells[column][row] >= 0.5) {
@@ -174,17 +150,18 @@ async function run() {
     cells = tcells;
   }
 }
-async function drawCells() {
+function drawCells() {
   if (!pause) {
     let canvas = document.getElementById("container").getContext("2d");
     for (let column = 0; column < cells.length; column++) {
       for (let row = 0; row < cells[column].length; row++) {
         if (
           canvas.fillStyle !=
-          "hsl(0, 0%, " + (-1 * cells[column][row] + 1) * 100 + "%)"
+          `hsl(0, 0%, ${(-1 * cells[column][row] + 1) * 100}%)`
         ) {
-          canvas.fillStyle =
-            "hsl(0, 0%, " + (-1 * cells[column][row] + 1) * 100 + "%)";
+          canvas.fillStyle = `hsl(0, 0%, ${
+            (-1 * cells[column][row] + 1) * 100
+          }%)`;
           canvas.fillRect(
             column * (800 / columns),
             row * (800 / rows),
@@ -195,4 +172,11 @@ async function drawCells() {
       }
     }
   }
+}
+function getMousePos(canvas, evt) {
+  var rect = canvas.getBoundingClientRect();
+  return {
+    x: evt.clientX - rect.left,
+    y: evt.clientY - rect.top,
+  };
 }
