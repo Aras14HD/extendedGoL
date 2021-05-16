6d70b913a3949e0c44dabe98508984c7452c20de
Aras <jorimbk@gmail.com>
2021-05-16 14:29:44 +0200

    compact convolution

--- Worker.js
+++ Worker.js
@@ -8,53 +8,31 @@ onmessage = function (e) {
     tcells.push([]);
     for (let row = 0; row < cells[column].length; row++) {
       let n = 0.0;
-      if (column > 0) {
-        if (row > 0) {
-          n += parseFloat(cells[column - 1][row - 1]);
-        } else {
-          n += parseFloat(cells[column - 1][rows - 1]);
+      for (let x = -1; x < 2; x++) {
+        for (let y = -1; y < 2; y++) {
+          if (x != 0 || y != 0) {
+            let neighbor_column, neighbor_row;
+
+            if (x == -1 && column == 0) {
+              neighbor_column = columns - 1;
+            } else if (x == 1 && column == columns - 1) {
+              neighbor_column = 0;
+            } else neighbor_column = column + x;
+
+            if (y == -1 && row == 0) {
+              neighbor_row = rows - 1;
+            } else if (y == 1 && row == rows - 1) {
+              neighbor_row = 0;
+            } else neighbor_row = row + y;
+
+            if (
+              cells[neighbor_column] != undefined &&
+              cells[neighbor_column][neighbor_row] != undefined
+            ) {
+              n += parseFloat(cells[neighbor_column][neighbor_row]);
+            }
+          }
         }
-        n += parseFloat(cells[column - 1][row]);
-        if (row < rows - 1) {
-          n += parseFloat(cells[column - 1][row + 1]);
-        } else {
-          n += parseFloat(cells[column - 1][0]);
-        }
-      } else {
-        if (row > 0) {
-          n += parseFloat(cells[columns - 1][row - 1]);
-        } else {
-          n += parseFloat(cells[columns - 1][rows - 1]);
-        }
-        n += parseFloat(cells[columns - 1][row]);
-        if (row < rows - 1) {
-          n += parseFloat(cells[columns - 1][row + 1]);
-        } else {
-          n += parseFloat(cells[columns - 1][0]);
-        }
-      }
-      if (row > 0) {
-        n += parseFloat(cells[column][row - 1]);
-      } else n += parseFloat(cells[column][rows - 1]);
-      if (row < rows - 1) {
-        n += parseFloat(cells[column][row + 1]);
-      } else n += parseFloat(cells[column][0]);
-      if (column < columns - 1) {
-        if (row > 0) {
-          n += parseFloat(cells[column + 1][row - 1]);
-        } else n += parseFloat(cells[column + 1][rows - 1]);
-        n += parseFloat(cells[column + 1][row]);
-        if (row < rows - 1) {
-          n += parseFloat(cells[column + 1][row + 1]);
-        } else n += parseFloat(cells[column + 1][0]);
-      } else {
-        if (row > 0) {
-          n += parseFloat(cells[0][row - 1]);
-        } else n += parseFloat(cells[0][rows - 1]);
-        n += parseFloat(cells[0][row]);
-        if (row < rows - 1) {
-          n += parseFloat(cells[0][row + 1]);
-        } else n += parseFloat(cells[0][0]);
       }
 
       if (cells[column][row] >= 0.5) {
